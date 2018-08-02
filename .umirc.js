244b3d20c3965d45d061e5c419547519db27ead1
wang <767074895@qq.com>
2018-08-03 01:11:13 +0800

    ''

--- .umirc.js
+++ .umirc.js
@@ -1,10 +1,8 @@
 export default {
   hashHistory: true,
-  es5ImcompatibleVersions: true,
   plugins: [
-        ['umi-plugin-dva',{ immer: true }],
-        'umi-plugin-polyfill',
-        'umi-plugin-es5-imcompatible-versions',
+    'umi-plugin-dva',
+    'umi-plugin-polyfill',
   ],
   hd:false, //放大了字体大小，图片没有bian
   "proxy": {        //配置代理
